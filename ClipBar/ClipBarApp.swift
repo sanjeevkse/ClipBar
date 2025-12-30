279b9361987566a0e9ef652c0702aaaef0ccdd5d
Sanjeev Shetty <sanjeevkse@gmail.com>
2025-12-30 09:41:28 +0530

    Initial Commit

---
+++ ClipBar/ClipBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBarApp.swift
+//  ClipBar
+//
+//  Created by Sanjeev on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
